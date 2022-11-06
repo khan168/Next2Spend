bf30c3ed7b37cb978934fdd6789d5812450a4058
Nhan <nhan.t.ngo@outlook.com>
2022-11-06 18:59:05 -0500

    =init react

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const PORT = process.env.port || 3001
-const app = express ()
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.listen(PORT, ()=> {
-    console.log(`Server listening on ${PORT}`)
-})  
+app.listen(PORT, () => {
+    console.log(`Server listen on ${PORT}`)
+})
