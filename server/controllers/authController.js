0c9a756cc7a928d2c8b8789927b99665b0b57ae9
khan162 <umarahmedkhank6@gmail.com>
2022-12-15 11:41:52 -0500

    removed async handlers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,59 +6,60 @@ require("dotenv").config();
 
 // @ desc       login user
 // @ router     router /api/user/login
-const login= asyncHandler(async (req,res)=>{
+const login= async (req,res)=>{
     const { email, password } = req.body;
-    const user = await User.findOne({email:email}) 
-
-    if(user && (await bcrypt.compare(password,user.password))){
-        res.json({
-            _id:user.id,
-            name:user.name,
-            email:user.email,
-            token:generateToken(user.id)
-        })
+    try{
+        const user = await User.findOne({email:email}) 
+        if(user && (await bcrypt.compare(password,user.password))){
+            res.json({
+                _id:user.id,
+                name:user.name,
+                email:user.email,
+                token:generateToken(user.id)
+            })
+        }
+        else{
+            res.status(400).json({error:"Invalid credentials"});
+        }
+    }catch(error){
+        res.status(400).json({ error: "Invalid credentials" });
     }
-    else{
-        res.status(400);
-        throw new Error("Invalid credentials");
-    }
-    
-});
+}
 
 // @ desc       sign up user
 // @ router     router /api/user/signup
-const  register = asyncHandler(async (req, res) => {
+const  register = async (req, res) => {
 
     const {name,email,password} = req.body;
     if(!email || !name || !password){
-        res.status(400)
-        throw new Error("please add all fields")
+        res.status(400).json({ error: "please add all fields" });
     }
-    const existinguser = await User.findOne({ email: email });
-    if (existinguser) {
-      res.status(400);
-      throw new Error("Invalid Credentials");
+    try{
+        const existinguser = await User.findOne({ email: email });
+        if (existinguser) {
+        res.status(400).json({error:"Invalid Credentials"})
+        }
+        const hashedPassword = await bcrypt.hash(password, 8);
+        const user = await User.create({
+            name,
+            email,
+            password:hashedPassword
+        })
+        if(user){
+            res.status(201).json({
+                _id:user.id,
+                name:user.name,
+                email:user.email,
+                token:generateToken(user.id)
+            }
+            )
+        }else{
+            res.status(400).json({error:"Invalid data"})
+        }
+    }catch(error){
+        res.status(400).json({ error: "Invalid Credentials" });
     }
-    const hashedPassword = await bcrypt.hash(password, 8);
-    const user = await User.create({
-        name,
-        email,
-        password:hashedPassword
-    })
-    if(user){
-        res.status(201).json({
-            _id:user.id,
-            name:user.name,
-            email:user.email,
-            token:generateToken(user.id)
-        }
-        )
-    }else{
-        res.status(400)
-        throw new Error("Invalid data")
-        }
-    
-});
+}
 
 // @ desc       get user data
 // @ router     router /api/user/getuser
