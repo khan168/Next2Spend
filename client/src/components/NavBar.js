5c754ead2a86b30a529d14007d28677b66a0405b
mishanguyen <nguyen14@usf.edu>
2023-01-18 23:10:27 -0500

    created the navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,19 @@ import '../styles/NavBar.css';
 function NavBar(){
     return (
         <div className='NavBar'>
-            <h1>Next2Spend</h1>
+            <div className='LogoContainer'>
+                <span>Next2Spend</span>
+            </div>
+            <div className='Buttons'>
+                <ul className='ButtonLinks'>
+                    <li className='ButtonLinkWrapper'>
+                        <a className='ButtonLink'>Log In</a>
+                    </li>
+                    <li className='ButtonLinkWrapper'>
+                        <a className='ButtonLink'>Sign Up</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
