b5e93f0de8d9236ad0d6755972d8047a4388b35d
mishanguyen <nguyen14@usf.edu>
2023-01-19 22:42:25 -0500

    created login and sign up pages

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,9 +1,27 @@
-
+import '../styles/LoginPage.css';
 
 function LoginPage(){
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className='LoginPage'>
+            <div className="LoginContainer">
+                <h1>Login</h1>
+                <div className="LoginForm">
+                    <form className="LogIn" action="/login" method="post">
+                        <div className='DataInput'>
+                            <label for="email">Email: </label>
+                            <input type="text" id="email" name="email" placeholder="janedoe@gmail.com" pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" required></input>
+                        </div>
+                        <div className='DataInput'>
+                            <label for="password">Password: </label>
+                            <input type="password" id="password" name="password" required></input>
+                        </div>
+                        <div className='LoginButton'>
+                            <input type="submit" value="Log in"></input>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -1,9 +1,38 @@
-// import '../styles/NavBar.css';
+import '../styles/SignUpPage.css';
 
 function SignUpPage(){
     return (
-        <div>
-            <h1>SignUp Page</h1>
+        <div class="SignUpPage">
+            <div className="SignUpContainer">
+                <h1>Sign Up</h1>
+                <div className="SignUpForm">
+                    <form className="SignUp" action="/signup" method="post">
+                        <div className='DataInput'>
+                            <label for="fname">First Name: </label>
+                            <input type="text" id="fname" name="fname" placeholder="Jane" required></input>
+                        </div>
+                        <div className='DataInput'>
+                            <label for="lname">Last Name: </label>
+                            <input type="text" id="lname" name="lname" placeholder="Doe" required></input>
+                        </div>
+                        <div className='DataInput'>
+                            <label for="email">Email: </label>
+                            <input type="text" id="email" name="email" placeholder="janedoe@gmail.com" pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" required></input>
+                        </div>
+                        <div className='DataInput'>
+                            <label for="password">Password: </label>
+                            <input type="password" id="password" name="password" required></input>
+                        </div>
+                        <div className='DataInput'>
+                            <label for="password">Confirm Password: </label>
+                            <input type="password" id="password" name="password" required></input>
+                        </div>
+                        <div className='SignUpButton'>
+                            <input type="submit" value="sign up"></input>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
