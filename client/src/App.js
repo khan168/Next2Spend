e7fabc8f64ec634188e9540b12f9d2f88f9d2298
mishanguyen <nguyen14@usf.edu>
2023-03-18 14:03:47 -0400

    added dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import SignUpPage from "./components/SignUpPage";
 import LoginPage from "./components/LoginPage";
 import Home from "./components/Home";
 import NavBar from './components/NavBar';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </Router>
   );
--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -2,30 +2,30 @@ import '../styles/SignUpPage.css';
 
 function SignUpPage(){
     return (
-        <div class="SignUpPage">
+        <div className="SignUpPage">
             <div className="SignUpContainer">
                 <h1>Sign Up</h1>
                 <div className="SignUpForm">
                     <form className="SignUp" action="/signup" method="post">
                         <div className='DataInput'>
-                            <label for="fname">First Name: </label>
+                            <label htmlFor="fname">First Name: </label>
                             <input type="text" id="fname" name="fname" placeholder="Jane" required></input>
                         </div>
                         <div className='DataInput'>
-                            <label for="lname">Last Name: </label>
+                            <label htmlFor="lname">Last Name: </label>
                             <input type="text" id="lname" name="lname" placeholder="Doe" required></input>
                         </div>
                         <div className='DataInput'>
-                            <label for="email">Email: </label>
+                            <label htmlFor="email">Email: </label>
                             <input type="text" id="email" name="email" placeholder="janedoe@gmail.com" pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" required></input>
                         </div>
                         <div className='DataInput'>
-                            <label for="password">Password: </label>
-                            <input type="password" id="password" name="password" required></input>
+                            <label htmlFor="password1">Password: </label>
+                            <input type="password" id="password1" name="password" required></input>
                         </div>
                         <div className='DataInput'>
-                            <label for="password">Confirm Password: </label>
-                            <input type="password" id="password" name="password" required></input>
+                            <label htmlFor="password2">Confirm Password: </label>
+                            <input type="password" id="password2" name="password" required></input>
                         </div>
                         <div className='SignUpButton'>
                             <input type="submit" value="sign up"></input>
