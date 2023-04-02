99426cf6745f170a037bddea94af2a2324e23bbd
khan162 <umarahmedkhank6@gmail.com>
2023-04-02 17:06:02 -0400

    user name added to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,23 +1,33 @@
+import { useState } from 'react';
 import '../styles/NavBar.css';
+import React from "react";
 
-function NavBar(){
+function NavBar(props){
+
     return (
-        <div className='NavBar'>
-            <div className='LogoContainer'>
-                <a href="/">Next2Spend</a>
-            </div>
-            <div className='Buttons'>
-                <ul className='ButtonLinks'>
-                    <li className='ButtonLinkWrapper'>
-                        <a href="/login" className='ButtonLink'>Log In</a>
-                    </li>
-                    <li className='ButtonLinkWrapper'>
-                        <a href="/signup" className='ButtonLink'>Sign Up</a>
-                    </li>
-                </ul>
-            </div>
+      <div className="NavBar">
+        <div className="LogoContainer">
+          <a href="/">Next2Spend</a>
         </div>
-    )
+        <div className="Buttons">
+          <ul className="ButtonLinks">
+            <li className="ButtonLinkWrapper">
+              <a href="/login" className="ButtonLink">
+                Log In
+              </a>
+            </li>
+            <li className="ButtonLinkWrapper">
+              <a href="/signup" className="ButtonLink">
+                Sign Up
+              </a>
+            </li>
+            {props.data &&<li className="ButtonLinkWrapper">
+                Hello {props.data.name}
+            </li>}
+          </ul>
+        </div>
+      </div>
+    );
 }
 
 export default NavBar;
